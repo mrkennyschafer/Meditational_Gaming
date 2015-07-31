480e2aa1bf673b5a6c53d40f2e5a04296c9e5980
NathanPussehl <NathanPussehl@fullsail.edu>
2015-07-30 22:46:49 -0400

    Physics
    Added the physic needed for the player and bubble.  Start working the
    the floating of the bubble

---
+++ Pop/Assets/_Scritps/Bubble/Floater.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Author: Nathan Pussehl
+// Date:
+//Purpose: Use to control the floating of the bubble
+
+public class Floater : MonoBehaviour 
+{
+	private Rigidbody2D bubbleRb2D;
+	public float speed;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	
+	}
+
+	void Awake()
+	{
+		bubbleRb2D = GameObject.FindGameObjectWithTag ("Bubble").GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		bubbleRb2D.AddForce(new Vector2(Random.Range(-10,10), 1f) * speed);
+	}
+}
---
+++ Pop/Assets/_Scritps/Player/PlayerTouchController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTouchController : MonoBehaviour 
+{
+
+	[SerializeField] private LayerMask whatIsGround; // Distigushes what is ground in this case bubble  "bubble have the layer mask asigned to them vis inspector" 
+	public Transform bubbleCheck; // transform use to detect if the player is touching the ground 
+	public float bubbleCheckRadius = 0.2f; // the radius to check for
+	
+	private Rigidbody2D playerRb2d; // will hold reference to the players Rigidbody2D Component
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+
+	void Awake()
+	{
+		// Gain reference to the rigidbody component of the player
+		playerRb2d = GameObject.FindGameObjectWithTag ("Player").GetComponent<Rigidbody2D> ();
+		// get the transform location of the ColliderCheck game object "Is a child of the Player"
+		bubbleCheck = transform.Find("BubbleCheck");
+	}
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	
+	
+	}
+
+	void FixedUpdate()
+	{
+
+		// Check to see if a bubble is overlaping the bubble check postion
+		if (Physics2D.OverlapCircle (bubbleCheck.position, bubbleCheckRadius, whatIsGround))
+		{
+			playerRb2d.mass = 0; //  remove players mass so they wont push the bubble down instead the bubble floats them up
+			Debug.Log("on Ground");
+		}
+		else 
+			playerRb2d.mass = 1f; // reset mass back to normal;
+			
+
+
+	}
+}
